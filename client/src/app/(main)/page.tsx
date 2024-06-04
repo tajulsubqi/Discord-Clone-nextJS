459f06cf11016ea209bf2b4e17e131711137df92
Tajul <subqitajul7@gmail.com>
2024-06-04 17:54:07 +0700

    feat: Dark & Light Theme setup

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { UserButton } from "@clerk/nextjs"
 import React from "react"
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
