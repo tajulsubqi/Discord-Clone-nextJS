853f4e77ccf5ae21d9379849e119887bf21c76ec
Tajul <subqitajul7@gmail.com>
2024-06-20 00:05:00 +0700

    feat: Chat Item component

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,10 @@
 import { ChatHeader } from "@/components/chat/chat-header"
 import { ChatInput } from "@/components/chat/chat-input"
+import { ChatMessages } from "@/components/chat/chat-messages"
 import { currentProfile } from "@/lib/current-profile"
-import { db } from "@/lib/db/db"
 import { redirectToSignIn } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { db } from "@/lib/db/db"
 
 interface ChannelIdPageProps {
   params: {
@@ -36,7 +37,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
 
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
 
       <ChatInput
         name={channel.name}
---
+++ src/components/providers/query-provider.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
